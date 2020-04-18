ce7ef4dc062987263b3f6e00566e9161d3e94c14
gdlopes <gustavodomlopes@gmail.com>
2020-04-18 10:47:07 -0300

    Complete challenge :rocket:

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# gostack11-desafio-02
+<img alt="GoStack" src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png" />
+
+<h3 align="center">
+  Desafio 02: Conceitos do Node.js
+</h3>
+
+## :rocket: Sobre o desafio
+
+Detalhes do desafio: https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs
+
+## :computer: Tecnologias utilizadas
+
+- [Node](https://nodejs.org/en/)
+- [Express](https://expressjs.com/)
+- [Nodemon](https://github.com/remy/nodemon)
+- [cors](https://www.npmjs.com/package/cors)
+- [uuidv4](https://www.npmjs.com/package/uuidv4)
+- [jest](https://jestjs.io/docs/en/getting-started.html)
+- [supertest](https://www.npmjs.com/package/supertest)
+
+## :warning: Pré-requisitos
+
+- [git](https://git-scm.com/)
+- [insomnia](https://insomnia.rest/)
+
+## :information_source: Instruções para rodar o projeto
+
+```bash
+# Clone this repository
+$ git clone https://github.com/gdlopes/gostack11-desafio-02.git
+
+# Go into the repository
+$ cd gostack11-desafio-02
+
+# Install dependencies
+$ yarn
+
+# Now, run the app to test the routes
+$ yarn dev
+
+```
+
+### :pencil2: Para rodar os testes, basta utilizar o seguinte comando:
+
+```bash
+  $ yarn test
+```
+
+## :boom: Agora, utilize o [Insomnia](https://insomnia.rest/) para ver tudo functionando :)
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,91 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (request, response) => {
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+  const { url, title, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    url,
+    title,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Its not a valid id' });
+  }
+
+  const repositoryIndex = repositories.findIndex(r => r.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: 'Repository not found' });
+  }
+
+  const { likes } = repositories[repositoryIndex];
+
+  const repository = {
+    id,
+    url,
+    title,
+    techs,
+    likes
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Its not a valid id' });
+  }
+
+  const repositoryIndex = repositories.findIndex(r => r.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: 'Repository not found' });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).json();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Its not a valid id' });
+  }
+
+  const repositoryIndex = repositories.findIndex(r => r.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: 'Repository not found' });
+  }
+
+  repositories[repositoryIndex].likes++;
+
+  const { likes } = repositories[repositoryIndex];
+
+  return response.json({ likes });
 });
 
 module.exports = app;
